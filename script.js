abb66b636491b6065b8f979531bd69d529eab6bc
AndresKingz <160375071+AndresKingz@users.noreply.github.com>
2024-02-17 23:07:03 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Aquí puedes agregar la lógica para validar el inicio de sesión
+    // Por ejemplo, puedes verificar las credenciales en el lado del cliente
+    // y mostrar un mensaje de inicio de sesión exitoso o fallido
+    if (username === "usuario" && password === "contraseña") {
+        document.getElementById("message").innerText = "Inicio de sesión exitoso";
+        // Redirigir al usuario a otra página después de iniciar sesión
+        // window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("message").innerText = "Credenciales incorrectas. Inténtalo de nuevo.";
+    }
+});
+
+document.getElementById("registerForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var regUsername = document.getElementById("regUsername").value;
+    var email = document.getElementById("email").value;
+    var regPassword = document.getElementById("regPassword").value;
+
+    // Aquí puedes agregar la lógica para registrar al usuario
+    // Por ejemplo, puedes enviar los datos del formulario a un servidor
+    // y manejar el registro allí, luego redirigir al usuario a la página de inicio de sesión
+    alert("Registrando usuario: " + regUsername + ", Correo electrónico: " + email + ", Contraseña: " + regPassword);
+});
